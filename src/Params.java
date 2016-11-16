91bbb4e3364a3203a6832040ff1edf82f560466b
Katarzyna Kosek <katkose@nuclab02.if.pw.edu.pl>
2016-11-16 10:50:21 +0100

    Add hamiltonian

--- src/Params.java
+++ src/Params.java
@@ -7,5 +7,9 @@ public class Params
 	public static final int n = 1;
 	public static final double x0  = 0;
 	public static final double x1 = 1;
+	public static final double kappa = 0;
+	public static final double tau = 0.001;
+	public static final double dTau = 0.001;
+	public static final double omega = 0.2;
 
 }
--- src/WaveFunction.java
+++ src/WaveFunction.java
@@ -1,29 +1,56 @@
 public class WaveFunction 
 {
 	private double [] points;
-	private double [] realValues;
-	private double [] imaginaryValues;
+	private double [] rValues;
+	private double [] iValues;
+	private double [] rHalfValues;
+	private double [] rHamiltionian;
+	private double [] iHamiltonian;
 	
 	private final double dx = 1/((double) Params.N);
 	
 	public WaveFunction() 
 	{
 		points = new double [Params.N + 1];
-		realValues = new double [Params.N + 1];
-		imaginaryValues = new double [Params.N + 1];
+		rValues = new double [Params.N + 1];
+		rHalfValues = new double [Params.N + 1];
+		iValues = new double [Params.N + 1];
 
 		for (int k=0; k<Params.N + 1; k++)
 		{
 			points[k] = k*dx; //(27)
-			realValues[k] = Math.sqrt(2)*
-					Math.sin(Params.n*Math.PI*points[k]);
-			imaginaryValues[k] = 0;
+			rValues[k] = Math.sqrt(2)*
+					Math.sin(Params.n*Math.PI*points[k]); //(29)
+			iValues[k] = 0; //(29)
+			rHamiltionian[k] = 0; 
+			iHamiltonian[k] = 0;
+		}
+	}
+
+	public void hamiltonian()
+	{
+		rHamiltionian[0] = 0;
+		rHamiltionian[Params.N + 1] = 0;
+		iHamiltonian[0] = 0;
+		iHamiltonian[Params.N + 1] = 0; //this four lines are from (31) border conditions
+		
+		for (int k=1; k<Params.N; k++)
+		{
+			rHamiltionian[k] = -0.5*(rValues[k+1]+rValues[k-1]-2*rValues[k])/(dx*dx)
+					+ Params.kappa*(points[k]-0.5)*rValues[k]*Math.sin(Params.tau*Params.omega); //(30)
+			
+			iHamiltonian[k] = -0.5*(iValues[k+1]+iValues[k-1]-2*iValues[k])/(dx*dx)
+					+ Params.kappa*(points[k]-0.5)*iValues[k]*Math.sin(Params.tau*Params.omega); //(30)
 		}
 	}
 	
-	public void update()
+	public void integrate()
 	{
-		
+		for (int k=0; k<Params.N + 1; k++)
+		{
+			rHalfValues[k] = rValues[k] + iHamiltonian[k]*Params.dTau*0.5; //32
+			
+		}
 	}
 
 }
